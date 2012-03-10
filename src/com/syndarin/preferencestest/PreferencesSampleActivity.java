4a65556c2ebefed52864301733a208ca1afdba4e
Syndarin <Syndarin@SyndarinDesktop>
2012-03-10 22:51:59 +0200

    Completed

--- src/com/syndarin/preferencestest/PreferencesSampleActivity.java
+++ src/com/syndarin/preferencestest/PreferencesSampleActivity.java
@@ -1,13 +1,47 @@
 package com.syndarin.preferencestest;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 
 public class PreferencesSampleActivity extends Activity {
     /** Called when the activity is first created. */
+	
+	Button buttonSettings;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        buttonSettings=(Button)findViewById(R.id.buttonSettings);
+        buttonSettings.setOnClickListener(new ButtonHandler());
     }
+    
+    
+    
+    @Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		SharedPreferences appSettings=PreferenceManager.getDefaultSharedPreferences(this);
+		String welcomeMessage=appSettings.getString("welcomeMessage", "none");
+		buttonSettings.setText(welcomeMessage);
+	}
+
+
+
+	private class ButtonHandler implements View.OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent i=new Intent(PreferencesSampleActivity.this, SettingsActivity.class);
+			PreferencesSampleActivity.this.startActivity(i);
+		}
+    }
+    
 }
---
+++ src/com/syndarin/preferencestest/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.syndarin.preferencestest;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+	
+	
+
+}
